3968b86db6f41bf49f20097ccb5f516050e1b6c9
doquochuy <doquochuyy66@gmail.com>
2024-11-27 09:27:44 +0700

    goole_dich

---
+++ src/main/java/com/example/baitapwebjsp/EnglishSevrlet.java
@@ -0,0 +1,36 @@
+package com.example.baitapwebjsp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "EnglishSevrlet", value = "/english")
+public class EnglishSevrlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String text = req.getParameter("text");
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("hello", "xin chao");
+        map.put("one", "1");
+        map.put("computer", "may tinh");
+        map.put("book", "sach");
+        map.put("product", "san pham");
+
+
+        if (text != null) {
+            if (map.containsKey(text)) {
+                resp.getWriter().write("Ket qua :  " + map.get(text));
+            } else{
+                resp.getWriter().write("Khong tim thay ket qua ");
+            }
+        } else {
+            resp.getWriter().write("Vui long nhap tu tra cuu");
+        }
+    }
+}
---
+++ src/main/webapp/English.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Dell
+  Date: 11/27/2024
+  Time: 8:57 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html; charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Google Dich</title>
+</head>
+<body>
+    <form action="english" method="get">
+        <input type="text" name="text" placeholder="enter your word : ">
+        <button type="submit" name="seach">Search</button>
+    </form>
+</body>
+</html>
