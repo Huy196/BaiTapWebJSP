b7154d370ff0a194a13681e7c3027250d3790333
doquochuy <doquochuyy66@gmail.com>
2024-11-27 08:52:56 +0700

    Tuan1/Calculator

---
+++ src/main/java/com/example/baitapwebjsp/ProductDiscountCalculator.java
@@ -0,0 +1,28 @@
+package com.example.baitapwebjsp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ProductDiscountCalculator",value = "/calculator")
+public class ProductDiscountCalculator extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String mota = req.getParameter("mota");
+        double price = Double.parseDouble(req.getParameter("number"));
+        int phanTram = Integer.parseInt(req.getParameter("phantram"));
+
+        float amonut = (float) (price * phanTram * 0.01);
+
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("<html>");
+        printWriter.println(mota);
+        printWriter.println("<h1>"+ "Kết quả : " + amonut + "</h1>");
+        printWriter.println("</html>");
+
+    }
+}
---
+++ src/main/webapp/Product_Discount_Calculator.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Dell
+  Date: 11/27/2024
+  Time: 8:27 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="calculator" method="get">
+    <header>Mô tả sản phẩm : </header> <input type="text" name="mota">
+    <header>Giá :</header> <input type="number" name="number" placeholder="Nhập giá....">
+    <header>Tỷ lệ chiết khấu :</header> <input type="number" name="phantram" placeholder=" ...2%...">
+    <button type="submit" name="calculator">Kết quả</button>
+</form>
+
+</body>
+</html>
